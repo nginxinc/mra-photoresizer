234902e267dfd12ac2fa3c719c2a0d3f9ed76d3d
Eduard Borges <eduardborges@me.com>
2018-05-29 10:28:54 +0200

    MRADEV-914 integrate proxy model and router mesh

--- app/start.sh
+++ app/start.sh
@@ -4,25 +4,40 @@ NGINX_CONF=""
 
 APP="java -jar target/PhotoResizer-1.0.1-SNAPSHOT.jar server PhotoResizer.yaml"
 
+${APP} &
+
+sleep 30
+APP_PID=`ps aux | grep "$APP" | grep -v grep`
+
+
 case "$NETWORK" in
     fabric)
         NGINX_CONF="/etc/nginx/fabric_nginx_$CONTAINER_ENGINE.conf"
         echo 'Fabric configuration set'
         nginx -c "$NGINX_CONF" -g "pid $NGINX_PID;" &
+
+        sleep 10
+
+        while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+        done
         ;;
     router-mesh)
+        while [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+        done
+        ;;
+    proxy)
+        while [ "$APP_PID" ];
+        do
+	        sleep 5;
+	        APP_PID=`ps aux | grep "$APP" | grep -v grep`;
+        done
         ;;
     *)
         echo 'Network not supported'
 esac
-
-$APP &
-
-sleep 30
-APP_PID=`ps aux | grep "$APP" | grep -v grep`
-
-while [ -f "$NGINX_PID" ] &&  [ "$APP_PID" ];
-do 
-	sleep 5;
-	APP_PID=`ps aux | grep "$APP" | grep -v grep`;
-done
