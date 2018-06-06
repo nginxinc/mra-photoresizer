f5bc7425b51c88ae04171bc60028f191be2d5c60
cstetson <chris.stetson@nginx.com>
2018-06-06 13:53:16 -0700

    restoring start

--- app/start.sh
+++ app/start.sh
@@ -2,7 +2,8 @@
 NGINX_PID="/var/run/nginx.pid"    # /   (root directory)
 APP="java -jar target/PhotoResizer-1.0.1-SNAPSHOT.jar server PhotoResizer.yaml"
 
-su resizer -c "${APP}" &
+
+su resizer --command "$APP" &
 
 sleep 30
 APP_PID=`ps aux | grep "$APP" | grep -v grep`
